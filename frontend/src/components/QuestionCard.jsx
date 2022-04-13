a51644574d22bdc4f2657df353341d05a58f874e
Serena71 <serena991031zs@gmail.com>
2022-04-13 22:42:22 +1000

    finished question card style and information display

--- frontend/src/components/QuestionCard.jsx
+++ frontend/src/components/QuestionCard.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useNavigate } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -8,6 +12,24 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { apiCall } from './Helper';
+import styles from './Style.module.css';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary
+}));
+
+// const colorPalette = [
+//   '#e4e9be',
+//   '#95d1cc',
+//   '#8d8daa',
+//   '#c69b7b',
+//   '#a2d5ab',
+//   '#fdd7aa'
+// ];
 
 export default function QuestionCard (props) {
   const navigate = useNavigate();
@@ -30,23 +52,56 @@ export default function QuestionCard (props) {
 
   const thisQuestion = props.quiz.questions[props.questionID];
   console.log(thisQuestion);
+
   return (
     <>
-      <Card sx={{ maxWidth: 345 }}>
+      <Card className={styles.space} sx={{ width: '80%', maxWidth: 345 }}>
         <CardContent>
+          {/* question title */}
           <Typography gutterBottom variant="h5" component="div">
-            Question Title
+            {thisQuestion.question}
           </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Question Content
-          </Typography>
+
+          {/* question metadata */}
+          <p>
+            Question Type:{' '}
+            {thisQuestion.type === 'Single choice'
+              ? (
+              <span>Single Choice</span>
+                )
+              : (
+              <span>Multiple Choice</span>
+                )}{' '}
+          </p>
+          <p>Time: {thisQuestion.duration} seconds</p>
+          <p>Credit: {thisQuestion.credit}</p>
+
+          {/* question uploaded media */}
+          <CardMedia
+            component="img"
+            alt="question media"
+            height="100"
+            image={thisQuestion.img}
+          />
+
+          {/* question options */}
+          <Box sx={{ width: '100%' }}>
+            <Stack spacing={2}>
+              {thisQuestion.options.map((opt, idx) => {
+                const correct = thisQuestion.correctAnswer[idx];
+                return (
+                  <Item
+                    className={correct ? styles.correctAnswer : undefined}
+                    key={idx}
+                  >
+                    {opt}
+                  </Item>
+                );
+              })}
+            </Stack>
+          </Box>
         </CardContent>
-        <CardMedia
-          component="img"
-          alt="question media"
-          height="100"
-          image="#"
-        />
+
         <CardActions>
           <Button size="small" color="error" onClick={deleteQuestion}>
             Delete
--- frontend/src/pages/QuizEdit.jsx
+++ frontend/src/pages/QuizEdit.jsx
@@ -4,8 +4,9 @@ import { NavTabs } from '../components/NavTab';
 import QuestionCard from '../components/QuestionCard';
 import { apiCall } from '../components/Helper';
 import styles from '../components/Style.module.css';
-
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import CircularProgress from '@mui/material/CircularProgress';
 
 export default function QuizEdit () {
   const navigate = useNavigate();
@@ -16,41 +17,57 @@ export default function QuizEdit () {
     }
   });
   const params = useParams();
+  const [loading, setLoading] = React.useState(false);
   const [quiz, setQuiz] = React.useState('');
   const [questions, setQuestions] = React.useState([]);
   const [questionDeleted, setQuestionDeleted] = React.useState(false);
   React.useEffect(() => {
+    setLoading(true);
     apiCall(`admin/quiz/${params.quizid}`, 'GET', {}).then((data) => {
       setQuiz(data);
       setQuestions(data.questions);
       setQuestionDeleted(false);
+      setLoading(false);
     });
   }, [questionDeleted]);
 
   return (
     <>
       <NavTabs />
-      <Button
-        className={styles.space}
-        variant="outlined"
-        onClick={() => {
-          navigate(`/quiz/${params.quizid}/${questions.length}`);
-        }}
-      >
-        Create New Question
-      </Button>
+      {loading && (
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
+        >
+          <CircularProgress />
+        </Box>
+      )}
 
-      {questions.map((q, idx) => {
-        return (
-          <QuestionCard
-            key={idx}
-            quizID={parseInt(params.quizid)}
-            questionID={idx}
-            quiz={quiz}
-            deleteLive={setQuestionDeleted}
-          />
-        );
-      })}
+      <div id="page" className={styles.pageAlign}>
+        <Button
+          className={styles.space}
+          variant="outlined"
+          onClick={() => {
+            navigate(`/quiz/${params.quizid}/${questions.length}`);
+          }}
+        >
+          Create New Question
+        </Button>
+        {questions.map((q, idx) => {
+          return (
+            <QuestionCard
+              key={idx}
+              quizID={parseInt(params.quizid)}
+              questionID={idx}
+              quiz={quiz}
+              deleteLive={setQuestionDeleted}
+            />
+          );
+        })}
+      </div>
     </>
   );
 }
